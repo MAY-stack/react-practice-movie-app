c237cd005df2a7ab09765c9316ee60c932c1de9e
Marisol <s.min.g7904@gamail.com>
2024-04-17 16:54:35 +0900

    initial commit

---
+++ src/components/Movie.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ id, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img alt="cover" src={coverImg} />
+      <Link to={`/movie/${id}`}>
+        <h2>{title}</h2>
+      </Link>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
